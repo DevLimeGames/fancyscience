2613bec34795211b372fd2bc78792154765130da
DevLimeGames <dev.limegames@gmail.com>
2025-03-02 10:20:26 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,3 @@
 Events.on(EventType.ClientLoadEvent, () => {
-    const menu = new MenuFragment((frag) => {
-        frag.table((t) => {
-            t.add("Fancy Science v0.3").pad(10);
-            t.row();
-            
-            t.row();
-            t.button("Close", () => {
-                menu.hide();
-            }).size(200, 60);
-        }).grow();
-    });
-
-    menu.show();
+    const menu =
 });
