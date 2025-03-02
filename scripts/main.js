e831a2a50b45b3e0dfd3de885de765e229a71639
DevLimeGames <dev.limegames@gmail.com>
2025-03-02 15:10:29 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,50 +1,44 @@
-Events.on(EventType.ClientLoadEvent, () => {
-    showFancyScienceMenu();
+Events.on(EventType.ClientLoadEvent, function() {
+    fetchReleaseDetails();
 });
 
-Events.on(EventType.MenuEvent, (event) => {
+Events.on(EventType.MenuEvent, function(event) {
     if (event.menu == MenuState.main) {
-        Time.runTask(10, showFancyScienceMenu);
+        Time.runTask(10, fetchReleaseDetails);
     }
 });
 
-function showFancyScienceMenu() {
-    Core.app.post(() => {
-        let dialog = new BaseDialog("FancyScience V0.4");
-        dialog.cont.add("Welcome to FancyScience!").padBottom(16).row();
+function fetchReleaseDetails() {
+    var currentVersion = "0.3";
+    var repo = "DevLimeGames/fancyscience";
+    var apiUrl = "https://api.github.com/repos/" + repo + "/releases/latest";
 
-        dialog.cont.button("Campaign", () => {
-            dialog.hide();
-            Vars.ui.campaign.show();
+    Http.get(apiUrl, function(res) {
+        var json = JSON.parse(res.getResultAsString());
+        if (json && json.name && json.body) {
+            showFancyScienceChangelog(currentVersion, json.name, json.body, json.html_url);
+        } else {
+            showFancyScienceChangelog(currentVersion, "Unknown", "No details available.", "https://github.com/DevLimeGames/fancyscience/releases/latest");
+        }
+    }, function(err) {
+        showFancyScienceChangelog(currentVersion, "Error", "Failed to load release details.", "https://github.com/DevLimeGames/fancyscience/releases/latest");
+    });
+}
+
+function showFancyScienceChangelog(currentVersion, latestVersion, releaseNotes, githubReleaseURL) {
+    Core.app.post(function() {
+        var dialog = new BaseDialog("FancyScience Changelog");
+        dialog.cont.add("[accent]FancyScience - Latest Release[/]").padBottom(16).row();
+        dialog.cont.add("[lightgray]Current Version: " + currentVersion + "[/]").padBottom(16).row();
+        dialog.cont.add("[lightgray]Latest Version: " + latestVersion + "[/]").padBottom(16).row();
+        dialog.cont.add(releaseNotes).width(400).wrap().padBottom(16).row();
+
+        dialog.cont.button("View on GitHub", function() {
+            Core.app.openURI(githubReleaseURL);
         }).size(220, 60).padBottom(16).row();
 
-        dialog.cont.button("Load Game", () => {
+        dialog.cont.button("Close", function() {
             dialog.hide();
-            Vars.ui.saves.show();
-        }).size(220, 60).padBottom(16).row();
-
-        dialog.cont.button("Custom Game", () => {
-            dialog.hide();
-            Vars.ui.custom.show();
-        }).size(220, 60).padBottom(16).row();
-
-        dialog.cont.button("Map Maker", () => {
-            dialog.hide();
-            Vars.ui.maps.show();
-        }).size(220, 60).padBottom(16).row();
-
-        dialog.cont.button("Mods", () => {
-            dialog.hide();
-            Vars.ui.mods.show();
-        }).size(220, 60).padBottom(16).row();
-
-        dialog.cont.button("Settings", () => {
-            dialog.hide();
-            Vars.ui.settings.show();
-        }).size(220, 60).padBottom(16).row();
-
-        dialog.cont.button("Quit", () => {
-            Core.app.exit();
         }).size(220, 60).padBottom(16).row();
 
         dialog.show();
