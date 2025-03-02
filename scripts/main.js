b39333ded9baa6eba7d199df5d4b198d271a7ad3
DevLimeGames <dev.limegames@gmail.com>
2025-03-02 14:38:21 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,12 @@ Events.on(EventType.ClientLoadEvent, () => {
         let dialog = new BaseDialog("FancyScience Updater");
         dialog.cont.add("Manage your FancyScience installation.").padBottom(32).row();
 
-        dialog.cont.button("Install Latest Release", () => {
-            installLatestRelease();
+        dialog.cont.button("Preview", () => {
+            openModBrowser("FancyScience");
         }).size(220, 60).padBottom(32).row();
 
-        dialog.cont.button("Install Preview", () => {
-            installPreview();
+        dialog.cont.button("Release", () => {
+            openModBrowser("FancyScience");
         }).size(220, 60).padBottom(32).row();
 
         dialog.cont.button("Close", () => {
@@ -19,31 +19,9 @@ Events.on(EventType.ClientLoadEvent, () => {
     });
 });
 
-function installLatestRelease() {
-    let latestDownload = "https://github.com/DevLimeGames/fancyscience/releases/latest/download/FancyScience.zip";
-    Vars.ui.showInfoToast("Downloading Latest Release...", 3);
-    downloadAndUpdateMod(latestDownload);
-}
-
-function installPreview() {
-    let previewDownload = "https://github.com/DevLimeGames/fancyscience/archive/main.zip";
-    Vars.ui.showInfoToast("Downloading Preview Version...", 3);
-    downloadAndUpdateMod(previewDownload);
-}
-
-function downloadAndUpdateMod(url) {
-    let modFolder = Vars.mods.getMod("fancyscience").file.parent(); 
-    let tempFile = modFolder.child("FancyScience_Temp.zip");
-
-    Http.get(url, (res) => {
-        let bytes = res.getResult();
-        if (bytes) {
-            tempFile.writeBytes(bytes, false);
-            Vars.ui.showInfoToast("Download complete! Restart the game to apply the update.", 4);
-        } else {
-            Vars.ui.showErrorMessage("Update failed!");
-        }
-    }, (err) => {
-        Vars.ui.showErrorMessage("Download error: " + err);
+function openModBrowser(modName) {
+    Vars.ui.mods.show();
+    Time.runTask(10, () => {
+        Vars.ui.mods.frag.search.setText(modName);
     });
 }
