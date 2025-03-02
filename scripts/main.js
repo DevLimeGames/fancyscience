ae56cd1b1b78d9a2ec9b115052e460a566eded46
DevLimeGames <dev.limegames@gmail.com>
2025-03-02 09:33:42 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 Events.on(EventType.ClientLoadEvent, () => {
-  const myDialog = new  BaseDialog("Dialog Title");
+  const myDialog = new BaseDialog("Dialog Title");
   // Add "go back" button
   myDialog.addCloseButton();
   // Add text to the main content
