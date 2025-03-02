bc964e4792561a3e999abd95546e4a58f759ce84
DevLimeGames <dev.limegames@gmail.com>
2025-03-02 09:48:49 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,33 @@
 Events.on(EventType.ClientLoadEvent, () => {
-  const myDialog = new BaseDialog("Fancy Science v0.3");
-  myDialog.addCloseButton();
-  myDialog.cont.add("Close");
-  myDialog.show();
+    const menu = new MenuFragment((frag) => {
+        frag.table((t) => {
+            t.add("Fancy Science v0.3").pad(10);
+            t.row();
+            
+            // Open Mod Browser to Fancy Science
+            t.button("Update Mod", () => {
+                Vars.ui.mods.show(); // Opens the mod browser
+                Time.runTask(10, () => {
+                    Vars.ui.mods.frag.toggle(); // Ensures the fragment is visible
+                    Vars.ui.mods.mods.each(mod => {
+                        if (mod.meta.name === "FancyScience") {
+                            Vars.ui.mods.showMod(mod); // Opens Fancy Science mod page
+                        }
+                    });
+                });
+            }).size(200, 60);
+            
+            t.row();
+            t.button("Reload Mod", () => {
+                Vars.mods.reload(); // Reloads all mods
+            }).size(200, 60);
+            
+            t.row();
+            t.button("Close", () => {
+                menu.hide();
+            }).size(200, 60);
+        }).grow();
+    });
+
+    menu.show();
 });
