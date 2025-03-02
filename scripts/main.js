ad24cce5e62c612f471cfd2b6fe13c610cc9287e
DevLimeGames <dev.limegames@gmail.com>
2025-03-02 14:45:44 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,46 @@
 Events.on(EventType.ClientLoadEvent, () => {
-    Core.app.post(() => {
-        let dialog = new BaseDialog("FancyScience Updater");
-        dialog.cont.add("Manage your FancyScience installation.").padBottom(32).row();
-
-        dialog.cont.button("Preview", () => {
-            openModBrowser("FancyScience");
-        }).size(220, 60).padBottom(32).row();
-
-        dialog.cont.button("Release", () => {
-            openModBrowser("FancyScience");
-        }).size(220, 60).padBottom(32).row();
-
-        dialog.cont.button("Close", () => {
-            dialog.hide();
-        }).size(220, 60).padBottom(32).row();
-
-        dialog.show();
-    });
+    showFancyScienceMenu();
 });
 
-function openModBrowser(modName) {
-    Vars.ui.mods.show();
-    Time.runTask(10, () => {
-        Vars.ui.mods.frag.search.setText(modName);
+Events.on(EventType.MenuEvent, (event) => {
+    if (event.menu == MenuState.main) {
+        Time.runTask(10, showFancyScienceMenu);
+    }
+});
+
+function showFancyScienceMenu() {
+    Core.app.post(() => {
+        let dialog = new BaseDialog("FancyScience V0.4");
+        dialog.cont.add("Welcome to FancyScience!").padBottom(16).row();
+
+        dialog.cont.button("Campaign", () => {
+            Vars.ui.loadAnd(MenuState.mode.campaign);
+        }).size(220, 60).padBottom(16).row();
+
+        dialog.cont.button("Load Game", () => {
+            Vars.ui.loadAnd(MenuState.mode.saves);
+        }).size(220, 60).padBottom(16).row();
+
+        dialog.cont.button("Custom Game", () => {
+            Vars.ui.loadAnd(MenuState.mode.custom);
+        }).size(220, 60).padBottom(16).row();
+
+        dialog.cont.button("Map Maker", () => {
+            Vars.ui.loadAnd(MenuState.mode.editor);
+        }).size(220, 60).padBottom(16).row();
+
+        dialog.cont.button("Mods", () => {
+            Vars.ui.mods.show();
+        }).size(220, 60).padBottom(16).row();
+
+        dialog.cont.button("Settings", () => {
+            Vars.ui.settings.show();
+        }).size(220, 60).padBottom(16).row();
+
+        dialog.cont.button("Quit", () => {
+            Core.app.exit();
+        }).size(220, 60).padBottom(16).row();
+
+        dialog.show();
     });
 }
